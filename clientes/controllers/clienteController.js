78c8611969d0fa8f52944bf3a1db40558550400d
Arthur Augusto <arthuraasantos@hotmail.com>
2016-08-18 23:13:03 -0300

    Iniciando respositorio 2

---
+++ clientes/controllers/clienteController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    caixaApp.controller('clienteController', function ($scope, $http, $rootScope, clienteService) {
+        $scope.listaDeClientes = clienteService.listarClientes();
+
+        $scope.editarCliente = function (cliente) {
+            $scope.contatoParaEditar = cliente;
+            $rootScope.exibirModal('visualizarContato');
+        }
+        
+    });
+})();
---
+++ clientes/services/clienteService.js
@@ -0,0 +1,43 @@
+﻿angular.module("caixaApp").factory("clienteService", function () {
+
+    var _listarClientes = function () {
+        var listaDeClientes = [
+        {
+            nome: "Maria",
+            email: "maria@gmail.com",
+            telefone: "21 1234 5678",
+            tipoCliente: true,
+            tipoFornecedor: false,
+            tipoParceiro: true
+        },
+        {
+            nome: "José",
+            email: "jose@gmail.com",
+            telefone: "21 2256 4887",
+            tipoCliente: false,
+            tipoFornecedor: false,
+            tipoParceiro: true
+        },
+        {
+            nome: "Carmem",
+            email: "carmem@gmail.com",
+            telefone: "21 3652 4004",
+            tipoCliente: true,
+            tipoFornecedor: true,
+            tipoParceiro: true
+        },
+        {
+            nome: "Pandora Dionesia",
+            email: "pandora@gmail.com",
+            telefone: "21 6395 2457",
+            tipoCliente: false,
+            tipoFornecedor: false,
+            tipoParceiro: true
+        }];
+        return listaDeClientes;
+    };
+
+    return {
+        listarClientes: _listarClientes
+    };
+});
