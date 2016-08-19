4e575e53a6079b8261ab1b822ac1f224a440a408
Arthur Augusto <arthuraasantos@hotmail.com>
2016-08-18 23:13:15 -0300

    Iniciando repositorio 3

---
+++ caixaApp.js
@@ -0,0 +1,65 @@
+﻿var caixaApp = angular.module('caixaApp', 
+[
+    'angularModalService',
+    'ui.router',
+    'ngRoute'
+])
+.run(function ($rootScope, $state, mensagemService, ModalService) {
+    $rootScope.exibirInformacao = function (mensagem) {
+        mensagemService.exibirInformacao(mensagem, 5);
+    };
+
+    $rootScope.exibirSucesso = function (mensagem) {
+        mensagemService.exibirSucesso(mensagem, 5);
+    };
+
+    $rootScope.exibirErro = function (mensagem) {
+        mensagemService.exibirErro(mensagem, 5);
+    };
+
+    $rootScope.exibirModal = function (template) {
+
+        if (!template) template = 'template.html';
+        ModalService.showModal({
+            templateUrl: template,
+            controller: "clienteController"
+        }).then(function (modal) {
+            modal.element.modal();
+            modal.close.then(function (result) {
+                $scope.exibirInformacao("Voce escolheu" + result);
+            });
+        });
+    };
+});
+
+caixaApp.config(['$routeProvider','$locationProvider', function(routeProvider, locationProvider){
+
+    locationProvider.html5Mode(true);
+
+    routeProvider.when('/', {
+        templateUrl: 'dashboard/dashboardTemplate.html',
+        controller: 'caixaAppController'
+    });
+
+    routeProvider.when('/clientes', {
+        templateUrl: 'clientes/templates/clientesTemplate.html',
+        controller: 'clienteController'
+    }).otherwise({ redirectTo: "/" });
+
+   
+
+
+
+}]);
+
+caixaApp.controller('caixaAppController', function ($scope, $http, $rootScope, mensagemService, ModalService) {
+    $scope.usuarioLogado = {
+        nome: "Mickey Mouse",
+        email: "mickey@disney.com.br",
+        telefone: "21 98653265"
+    };
+
+  
+});
+
+
---
+++ mensagemService.js
@@ -0,0 +1,41 @@
+﻿    angular.module("caixaApp").factory("mensagemService", function () {
+
+        var _exibirInformacao = function (mensagem, tempoExibindo) {
+            _exibirMensagem(mensagem,'info',tempoExibindo);
+        };
+
+        var _exibirErro = function (mensagem, tempoExibindo) {
+            _exibirMensagem(mensagem, 'error',tempoExibindo);
+        };
+
+        var _exibirSucesso = function (mensagem, tempoExibindo) {
+            _exibirMensagem(mensagem, 'success',tempoExibindo);
+        };
+
+
+        var _exibirMensagem = function (mensagem, tipo, tempoExibindo) {
+            if (mensagem !== null) {
+
+                if (tipo === null) tipo = 'info';
+                if (tempoExibindo == null) tempoExibindo = 10;
+
+                Messenger.options = {
+                    extraClasses: 'messenger-fixed messenger-on-top messenger-on-center',
+                    theme: 'air'
+                };
+
+                Messenger().post({
+                    message: mensagem,
+                    type: tipo,
+                    showCloseButton: true,
+                    hideAfter: tempoExibindo
+                });
+            }
+        };
+
+        return {
+            exibirInformacao: _exibirInformacao,
+            exibirErro: _exibirErro,
+            exibirSucesso: _exibirSucesso
+        };
+    });
